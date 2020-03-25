43e7410be645fb3b1d93201e10c5dfb0cdc771c2
Catherine Gomes <catherinegomes@users.noreply.github.com>
2020-03-25 14:24:38 -0500

    fix dtype in class and remove typo in load_to_mysql

--- static/js/aus_fire_history.js
+++ static/js/aus_fire_history.js
@@ -1,50 +1,49 @@
-// TBD
+// This function will build the Australia Map and display locations of fires
+function buildAUSmap(totalData) {
 
-// let newPlot = d3.select('plot')
-let loadFile = Papa.parse("data/aus_fire_locations/australia.csv", {
-	download: true,
-	step: function(row) {
-		console.log("Row:", row.data);
-	},
-	complete: function() {
-		console.log("All done!");
-	}
-});
-Plotly.d3.csv('data/aus_fire_locations/australia.csv',
-	function(err, rows) {
-		function unpack(rows, key) {
-			return rows.map(function(row) {
-				return row[key];
-			});
-		}
+	let trace1 = {
+	    lat: totalData.map(e => e.latitude),
+	    lon: totalData.map(e => e.longitude),
+        text: "bright_ti4",
+	    type: "scattermapbox",
+	   	marker: {color: "red", size: 4}
+	};
+	
+	let plotData = [trace1]
 
-        var data = [
-            {
-                type: "scattermapbox",
-                text: unpack(rows, "bright_ti4"),
-                lon: unpack(rows, "longitude"),
-                lat: unpack(rows, "latitude"),
-                marker: { color: "fuchsia", size: 4 }
-            }
-        ];
+    let layout = {
+        dragmode: "zoom",
+        mapbox: {
+            style: "white-bg",
+            layers: [
+                {
+                    sourcetype: "raster",
+                    source: ["https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryOnly/MapServer/tile/{z}/{y}/{x}"],
+                    below: "traces"
+                }
+            ],
+            center: { lat: -24.2, lon: 135 },
+            zoom: 3
+        },
+        margin: { r: 0, t: 0, b: 0, l: 0 }
+    };
 
-        var layout = {
-            dragmode: "zoom",
-            mapbox: {
-                style: "white-bg",
-                layers: [
-                    {
-                        sourcetype: "raster",
-                        source: ["https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryOnly/MapServer/tile/{z}/{y}/{x}"],
-                        below: "traces"
-                    }
-                ],
-                center: { lat: -24.2, lon: 135 },
-                zoom: 3
-            },
-            margin: { r: 0, t: 0, b: 0, l: 0 }
-        };
+    Plotly.newPlot("plot", plotData, layout)
+}
 
-Plotly.newPlot('plot', data, layout);
-	}
-);
+// This function will take the data generated from the html route
+// plug it into the above function and plot as desired
+function init() {
+
+	console.log("hello")
+
+    // Contains the JSON version of data
+	d3.json("/aus_fire_history").then((data) => {
+
+		console.log("hello again")
+        buildAUSmap(data)
+    })
+
+}
+
+init();
