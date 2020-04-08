00540eede1a579598860cd6d5d7ca8e5ea117dc3
Ekta Sharma <ekta.sharma3@gmail.com>
2020-04-07 20:09:03 -0500

    added 404 route

--- app.py
+++ app.py
@@ -664,9 +664,9 @@ def climate_data():
 #                       Custom 404 Page                             #
 #####################################################################
 
-# @app.errorhandler(404)
-# def page_not_found(e):
-#     return render_template('404.html'), 404
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 
 #####################################################################
